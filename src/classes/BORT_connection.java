6be99d683c0ce1cb0d483d34ebd075bdb6120c46
Кирилл Малякин <malyakin-10@yandex.ru>
2018-07-27 23:52:35 +0300

    Наконец переписан комментарий, небольшие дополнения.

--- src/classes/BORT_connection.java
+++ src/classes/BORT_connection.java
@@ -11,24 +11,24 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 /**
- * Данный класс представляет собой обработчик модуля инфракрасного приёмника для ИК - пультов.
- * Модуль инфракрасного приёмника представляет собой плату Arduino Nano c ИК - детектором и миниатюрным динамиком.
+ * Данный класс позволяет представить соединение с модулем бортового компьютера в виде объекта.
+ * Соединение - это виртуальный COM - порт, через который программма обменивается командами и данными с модулем.
  * Микроконтроллер передаёт данные через UART (последовательный порт TTL - уровня), а они, в свою очередь, принимаются
  * установленной на плате микросхемой - конвертором, которая подключается к компьютеру с помощью USB.
  * В конечном итоге, в системе данные можно принять из виртуального эмулированного COM - порта.
  * Данный класс использует библиотеку JSSC для работы с последовательным портом. Это библиотека с открытым исходным кодом,
  * позволяющая организовать многопоточную асинхронную работу с COM - портами.
- * Класс принимает данные с модуля, позволяет подключить обработчики событий нажатия на кнопки, принимать одиночные нажатия,
- * чтобы организовать, например, ввод определённх ожидаемых клавиш.
+ * Класс принимает данные с модуля и складывает их в стек полученных ответов, из которого они могут быть доступны в дальнейшем.
  * Содержит методы, позволяющие полноценно работать с модулем, а также реализует интерфейс Closeable, поэтому его можно
  * использовать в конструкциях try с ресурсами.
  */
 public class BORT_connection implements SerialPortEventListener, Closeable {
 
-    private final ArrayList<BORT_response> responsesStack = new ArrayList<>();
     private final SerialPort serialPort;
     private final StringBuilder receivingBuf = new StringBuilder();
     private final DebugInformationListener debugInformationListener;
+    @SuppressWarnings("CanBeFinal")
+    private volatile ArrayList<BORT_response> responsesStack = new ArrayList<>();
     private boolean isConnected = false;
 
     /**
@@ -61,7 +61,7 @@ public class BORT_connection implements SerialPortEventListener, Closeable {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        return "";
+        return "error";
     }
 
     /**
@@ -98,6 +98,7 @@ public class BORT_connection implements SerialPortEventListener, Closeable {
         switch (gettedResponse.getResponseType()) {
             case MODULE_STARTED:
                 isConnected = true;
+                send(CommandsConstList.CMD_CONNECTION_APPROVED);
                 break;
             case DEBUG_INFORMATION:
                 debugInformationListener.debugInformationGetted(gettedResponse);
